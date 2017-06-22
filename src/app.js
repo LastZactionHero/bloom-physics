24227c89177cbb4cbfc2770a6dbdd3c766ebbe5a
= <=>
2017-06-22 14:13:42 -0600

    Headless render; Dividers

--- src/app.js
+++ src/app.js
@@ -20,77 +20,77 @@ var render = Render.create({
 const WORLD_WIDTH = 800;
 const WORLD_HEIGHT = 600;
 
+const boundaryFloor = Bodies.rectangle(WORLD_WIDTH / 2, WORLD_HEIGHT - 1, WORLD_WIDTH, 100, { isStatic: true });
+const boundaryLeft = Bodies.rectangle(0, WORLD_HEIGHT / 2, 100, WORLD_HEIGHT, { isStatic: true });
+const boundaryRight = Bodies.rectangle(WORLD_WIDTH - 1, WORLD_HEIGHT / 2, 100, WORLD_HEIGHT, { isStatic: true });
+
+const dividers = [
+  Bodies.rectangle(WORLD_WIDTH / 3, WORLD_HEIGHT / 2, 10, WORLD_HEIGHT, { isStatic: true }),
+  Bodies.rectangle(WORLD_WIDTH - WORLD_WIDTH / 3, WORLD_HEIGHT / 2, 10, WORLD_HEIGHT, { isStatic: true })
+]
+
+
 World.add(engine.world, [
-    Bodies.rectangle(WORLD_WIDTH / 2, WORLD_HEIGHT - 1, WORLD_WIDTH, 10, { isStatic: true }),
-    Bodies.rectangle(0, WORLD_HEIGHT / 2, 1, WORLD_HEIGHT, { isStatic: true }),
-    Bodies.rectangle(WORLD_WIDTH - 1, WORLD_HEIGHT / 2, 1, WORLD_HEIGHT, { isStatic: true }),
+    boundaryFloor,
+    boundaryLeft,
+    boundaryRight
+
 ]);
+World.add(engine.world, dividers);
 
 
-
-window.MatterAttractors = MatterAttractors;
-MatterAttractors.Attractors.gravityConstant = 0.01;
-
-const ATTRACTOR_RADIUS = 10
-const leftCornerAttractorBody = Bodies.circle(ATTRACTOR_RADIUS * 1.5, WORLD_HEIGHT + ATTRACTOR_RADIUS, ATTRACTOR_RADIUS, {
-  isStatic: true,
-  mass: 1000,
-  plugin: {
-    attractors: [
-      MatterAttractors.Attractors.gravity
-    ]
-  }
-});
-
-const rightCornerAttractorBody = Bodies.circle(WORLD_WIDTH - ATTRACTOR_RADIUS * 1.5, WORLD_HEIGHT + ATTRACTOR_RADIUS, ATTRACTOR_RADIUS, {
-  isStatic: true,
-  mass: 1000,
-  plugin: {
-    attractors: [
-      MatterAttractors.Attractors.gravity
-    ]
-  }
-});
-
-World.add(engine.world, leftCornerAttractorBody)
-World.add(engine.world, rightCornerAttractorBody)
+const CIRCLE_RADIUS = 10;
 
 for(var i = 0; i < 20; i++) {
-  const xPosn = Math.random() * (WORLD_WIDTH - 2 * ATTRACTOR_RADIUS) + ATTRACTOR_RADIUS;
+  const xPosn = i % 2 == 0 ? 100 + 5 * Math.random() : WORLD_WIDTH - 100 - 5 * Math.random();
   const yPosn = 200 - Math.random() * WORLD_HEIGHT;
-  const circleBody = Bodies.circle(xPosn, yPosn, 20, { label: 'my circle'});
-  World.add(engine.world, circleBody)  
+
+  const circleBody = Bodies.circle(xPosn, yPosn, 20, { label: 'my circle', mass: 100});
+  World.add(engine.world, circleBody)
 }
-// const circleDiameter = 20;
-// const launchOffset = 10;
-// for(var i = 0; i < 20; i++) {
-//     const circle = Bodies.circle(400 + i, launchOffset - i * circleDiameter, circleDiameter, { label: 'circle-left'})
-//     World.add(engine.world, circle)
-// }
 
 engine.world.gravity.x = 0;
-engine.world.gravity.y = 0.01;
+engine.world.gravity.y = 0.1;
 
-// let sideChangeTriggered = false;
-// Events.on(engine, 'afterUpdate', function(timestamp) {
-//     if(timestamp.timestamp > 5000 && !sideChangeTriggered) {
-//         sideChangeTriggered = true;
-//         engine.world.bodies.forEach( function(body) {
-//             if(body.label == 'circle-left') {
-//                 body.isStatic = true;
-//             }
-//         });
+const keyframes = [
+  { timestamp: 2000, action: 'remove_dividers', used: false}
+];
 
-//         engine.world.gravity.x = 2;
-//         for(var i = 0; i < 20; i++) {
-//             const circle = Bodies.circle(400 - i, launchOffset - i * circleDiameter, circleDiameter, { label: 'circle-left'})
-//             World.add(engine.world, circle)
-//         }
-//     }
-// })
+Events.on(engine, "afterUpdate", function(event) {
+  keyframes.forEach( function(keyframe) {
+    if(keyframe.timestamp < event.timestamp && !keyframe.used) {
+      keyframe.used = true;
+      console.log("Keyframe Event:")
+      console.log(keyframe)
+
+      switch(keyframe.action) {
+        case 'remove_dividers':
+          // World.remove(engine.world, dividers)
+          break;
+        default:
+          console.log("Unknown keyframe event!")
+          break;
+      }
+    }
+  })
+});
+
 
 // run the engine
-Engine.run(engine);
+for(i = 0; i < 5000; i++) {
+  Engine.update(engine)
+}
+// World.remove(engine.world, dividers)
+World.add(engine.world, [
+  Bodies.circle(150, 400, 60, {isStatic: true}),
+  Bodies.circle(400, 400, 60, {isStatic: true}),
+  Bodies.circle(WORLD_WIDTH - 150, 400, 60, {isStatic: true}),
+])
+for(i = 0; i < 5000; i++) {
+  Engine.update(engine)
+}
+World.remove(engine.world, dividers)
+// Engine.run(engine); 
 
 // run the renderer
 Render.run(render);
